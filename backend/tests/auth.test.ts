852d0e387beac7456844b91333099628e1927c35
ldickmann <ldickmann12@gmail.com>
2025-11-19 06:57:18 -0300

    Feat: Adiciona testes para rotas de autenticação com registro e login de usuários

---
+++ backend/tests/auth.test.ts
@@ -0,0 +1,44 @@
+import request from 'supertest';
+import { app } from '../src/app.js';
+import { prisma } from '../src/utils/prismaClient.js';
+
+describe('Auth - /auth', () => {
+  const user = {
+    name: 'Test User',
+    email: 'test@example.com',
+    password: 'Senha123!'
+  };
+
+  beforeAll(async () => {
+    await prisma.user.deleteMany(); // limpa o banco antes de iniciar
+  });
+
+  afterAll(async () => {
+    await prisma.$disconnect();
+  });
+
+  it('should register a user and return 201', async () => {
+    const res = await request(app).post('/auth/register').send(user);
+
+    expect(res.status).toBe(201);
+    expect(res.body).toHaveProperty('id');
+    expect(res.body.email).toBe(user.email);
+  });
+
+  it('should login and return a valid JWT token', async () => {
+    await request(app).post('/auth/register').send({
+      name: 'LoginTeste',
+      email: 'login@example.com',
+      password: 'Senha123!'
+    });
+
+    const res = await request(app).post('/auth/login').send({
+      email: 'login@example.com',
+      password: 'Senha123!'
+    });
+
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty('token');
+    expect(typeof res.body.token).toBe('string');
+  });
+});
